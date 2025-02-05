1454f95a9ae749be4b7a5c72baf1baa1e60399bf
Ali <biz.alihabibi@gmail.com>
2025-02-05 19:34:24 +0330

    Initialize View Binding

--- app/src/main/java/com/sample/ali/goldprice/MainActivity.kt
+++ app/src/main/java/com/sample/ali/goldprice/MainActivity.kt
@@ -5,13 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sample.ali.goldprice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindind: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        bindind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindind.root)
+        ViewCompat.setOnApplyWindowInsetsListener(bindind.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
