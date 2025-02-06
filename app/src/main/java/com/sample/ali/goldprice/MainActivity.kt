65157a65c823ecf56243612ca47ca5dbc8b26b55
Ali <biz.alihabibi@gmail.com>
2025-02-06 16:52:52 +0330

    Final Splash Screen

--- app/src/main/java/com/sample/ali/goldprice/MainActivity.kt
+++ app/src/main/java/com/sample/ali/goldprice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sample.ali.goldprice
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.sample.ali.goldprice.databinding.ActivityMainBinding
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bindind: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         bindind = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindind.root)
