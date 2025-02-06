942605697d68e111bc1993450c1ef04d8bb27b12
Ali <biz.alihabibi@gmail.com>
2025-02-06 17:35:45 +0330

    Splash Screen Waits Till Activity Is Ready

--- app/src/main/java/com/sample/ali/goldprice/MainActivity.kt
+++ app/src/main/java/com/sample/ali/goldprice/MainActivity.kt
@@ -6,20 +6,34 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.sample.ali.goldprice.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bindind: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+        var isActivityReady = false
+        installSplashScreen().setKeepOnScreenCondition { !isActivityReady }
         enableEdgeToEdge()
-        bindind = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bindind.root)
-        ViewCompat.setOnApplyWindowInsetsListener(bindind.root) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                isActivityReady = loadData()
+            }
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun loadData(): Boolean {
+        return true
     }
 }
