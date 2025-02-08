1c159c28de49de8383aaa9eba3aafa48176c820e
Ali <biz.alihabibi@gmail.com>
2025-02-08 13:33:01 +0330

    Design Currency Price Recycler View List Item

--- app/src/main/java/com/sample/ali/goldprice/MainActivity.kt
+++ app/src/main/java/com/sample/ali/goldprice/MainActivity.kt
@@ -7,12 +7,8 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
 import com.google.android.material.tabs.TabLayoutMediator
 import com.sample.ali.goldprice.databinding.ActivityMainBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -26,24 +22,20 @@ class MainActivity : AppCompatActivity() {
         insetsController.isAppearanceLightStatusBars = false
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        lifecycleScope.launch {
-            withContext(Dispatchers.IO) {
-                isActivityReady = loadData()
-            }
-        }
+        isActivityReady = loadActivity()
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun loadActivity(): Boolean {
         binding.tabLayoutViewPager.adapter = TabLayoutAdapter(supportFragmentManager, lifecycle)
         val tabLayoutTitles = arrayListOf("قیمت طلا", "قیمت ارز")
         TabLayoutMediator(binding.tabLayout, binding.tabLayoutViewPager) { tab, position ->
             tab.text = tabLayoutTitles[position]
         }.attach()
-    }
-
-    private fun loadData(): Boolean {
         return true
     }
 }
